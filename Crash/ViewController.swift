116ada8cbe685c094d935d5026ee080e8cd4af7f
Sebastian Grail <sgrail@gmail.com>
2015-10-31 11:51:26 +1100

    Initial commit

---
+++ Crash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Crash
+//
+//  Created by Sebastian Grail on 30/10/2015.
+//  Copyright © 2015 Sebastian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Break here and run a debugger command, e.g. 'p 1' => crash
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# Xcode 7.1 Crash
+
+## Steps to reproduce:
+
+1. Run the project
+2. When the shared breakpoint is hit, run `po self` or any other `expression` command in the debugger
+3. There is no step 3 (because Xcode crashed)
+
